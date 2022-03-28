30522e17fcbf462ecfe4f8e73b622c77e484c216
alexeim <alexeim.dev@gmail.com>
2022-03-28 20:58:51 +0300

    reafactoring

--- server/api/products.js
+++ server/api/products.js
@@ -1,20 +1,20 @@
 const { Router } = require('express');
 const EShop = require('../databases/eshop');
 
-const productsRouter = Router();
+const router = Router();
 
 const eShop = new EShop();
 
-productsRouter.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
     const allProducts = await eShop.getAllProducts();
     console.info('[server]', 'allProducts', allProducts);
     res.json(allProducts);
 });
 
-productsRouter.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const product = await eShop.getProduct(req.params.id);
     console.info('[server]', 'product', product);
     res.json(product);
 });
 
-module.exports = productsRouter;
+module.exports = router;
